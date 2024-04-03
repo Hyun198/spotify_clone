de5234b7f54c535af627e8fda39234c583045015
Hyun <hwn123h@naver.com>
2024-04-03 23:37:56 +0900

    track 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import { render } from '@testing-library/react';
 function App() {
 
   const CLIENT_ID = "28e364844a4848e3905c1f15953abfd0"
@@ -13,6 +12,7 @@ function App() {
   const [searchKey, setSearchKey] = useState("")
   const [topArtist, setTopArtist] = useState(null)
   const [artists, setArtists] = useState([])
+  const [tracks, setTracks] = useState([])
 
   useEffect(() => {
     const hash = window.location.hash
@@ -34,28 +34,36 @@ function App() {
   }
 
   const getTracks = async (artistId) => {
-    const { data } = await axios.get(`https://api.spotify.com/v1/artists/${artistId}`, {
+    const { data } = await axios.get(`https://api.spotify.com/v1/artists/${artistId}/top-tracks`, {
       headers: {
         Authorization: `Bearer ${token}`
       },
     });
-    console.log(1);
-    console.log(data);
+    const tracks = data.tracks;
+    setTracks(tracks);
   }
 
   const searchArtists = async (e) => {
     e.preventDefault()
+
+    const searchTerm = searchKey.trim()
+
+    if (!searchTerm) {
+      alert("검색어를 입력하세요.");
+      return;
+    }
     const { data } = await axios.get("https://api.spotify.com/v1/search", {
       headers: {
         Authorization: `Bearer ${token}`
       },
       params: {
-        q: searchKey,
+        q: searchTerm,
         type: "artist",
         limit: 10
       }
     })
     const topArtist = data.artists.items[0];
+    console.log(topArtist);
     setTopArtist(topArtist)
     setArtists(data.artists.items)
 
@@ -84,7 +92,6 @@ function App() {
       <div className="App">
         <h1 className='title'>Spotify Clone</h1>
         <header className="header_container">
-
           {token ?
             <form onSubmit={searchArtists}>
               <input className="search_box" type="text" placeholder='노래, 앨범, 아티스트 검색' onChange={e => setSearchKey(e.target.value)} />
@@ -94,15 +101,33 @@ function App() {
           }
           {!token ? <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
             : <button className="btn-logout" onClick={logout}>logout</button>}
-
-
         </header>
-        {topArtist && (
-          <div className='top-artist'>
-            {topArtist.images.length ? <img className="artist_imgs" src={topArtist.images[0].url} alt={topArtist.name} /> : <div>No images</div>}
-            <h2>{topArtist.name}</h2>
+        <main className="main_container">
+          {topArtist && (
+            <div className='top-artist'>
+              {topArtist.images.length ? <img className="topArtist_imgs" src={topArtist.images[0].url} alt={topArtist.name} /> : <div>No images</div>}
+
+              <h2>{topArtist.name}</h2>
+              <p className='followers'>팔로워 수: {topArtist.followers.total}</p>
+              <div className='top-artist-info'>
+                {topArtist.genres.map(genre => (
+                  <p className='genre'>{genre}</p>
+                ))}
+              </div>
+
+            </div>
+          )}
+          <div className="top-tracks">
+            {tracks.slice(0, 7).map(track => (
+              <div className="track" key={track.id}>
+                <img className="track_imgs" src={track.album.images[0].url} alt={track.name} />
+                <div className='track-info'>
+                  <h2>{track.name}</h2>
+                </div>
+              </div>
+            ))}
           </div>
-        )}
+        </main>
 
         <h3>아티스트</h3>
         <div className="artists">
